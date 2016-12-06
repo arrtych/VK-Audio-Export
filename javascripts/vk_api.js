2ee4675b95b49d80bf81385a6f8149ca3e88e282
4matic <maksim93@mail.ru>
2016-12-06 02:44:07 +0200

    Unknown auth type received. Bug

--- javascripts/vk_api.js
+++ javascripts/vk_api.js
@@ -1,26 +1,31 @@
 /**
  * Created by 4Matic on 6.12.2016.
  */
-window.vkAsyncInit = function() {
-    VK.init({
-        apiId: 5763233
-    });
-    VK.Widgets.Like("vk_like", {type: "button"});
-    VK.Observer.subscribe("widgets.like.liked", function(){
-        alert("Thank you for your like.");
-    });
-};
+VK.init({
+    apiId: 5763233
+});
+VK.Widgets.Like("vk_like", {type: "button"});
+VK.Observer.subscribe("widgets.like.liked", function(){
+    alert("Thank you for your like.");
+});
+VK.Observer.subscribe("auth.login", function(e){
+    alert("auth.login");
+    console.log(e);
+});
+function authInfo(response) {
+    if (response.session) {
+        alert('user: '+response.session.mid);
+    } else {
+        alert('not auth');
+    }
+}
+VK.Widgets.Auth("vk_auth", {width: "200px", onAuth: function(data) {
+    alert('user '+data['uid']+' authorized');
+} });
+VK.Auth.getLoginStatus(authInfo);
+// VK.UI.button('login_button');
 function initLogin() {
-    VK.Auth.login(function (session, status) {
-        if (status == 'connected') {
-            alert('connected');
-        } else if (status == 'not_authorized') {
-            alert('not_authorized');
-        } else {
-            alert('unknown');
-        }
-        console.log(session, status);
-    }, 8);
+    VK.Auth.login(authInfo, 8);
 }
 $(document).ready(function(){
     $('.login-btn').on('click', function(){
