84d045e9e225220aabf1c79e25164d8f59c36d0d
4matic <maksim93@mail.ru>
2016-12-06 03:15:23 +0200

    Some test commit. Auth not working :(

--- javascripts/vk_api.js
+++ javascripts/vk_api.js
@@ -1,31 +1,34 @@
 /**
  * Created by 4Matic on 6.12.2016.
  */
-VK.init({
-    apiId: 5763233
-});
-VK.Widgets.Like("vk_like", {type: "button"});
-VK.Observer.subscribe("widgets.like.liked", function(){
-    alert("Thank you for your like.");
-});
-VK.Observer.subscribe("auth.login", function(e){
-    alert("auth.login");
-    console.log(e);
-});
-function authInfo(response) {
-    if (response.session) {
-        alert('user: '+response.session.mid);
-    } else {
-        alert('not auth');
-    }
-}
-VK.Widgets.Auth("vk_auth", {width: "200px", onAuth: function(data) {
-    alert('user '+data['uid']+' authorized');
-} });
-VK.Auth.getLoginStatus(authInfo);
-// VK.UI.button('login_button');
+window.vkAsyncInit = function() {
+    VK.init({
+        apiId: 5763233
+    });
+    VK.Widgets.Like("vk_like", {type: "button"});
+    VK.Observer.subscribe("widgets.like.liked", function(){
+        alert("Thank you for your like.");
+    });
+    VK.Observer.subscribe("auth.login", function(e){
+        alert("auth.login");
+        console.log(e);
+    });
+};
 function initLogin() {
-    VK.Auth.login(authInfo, 8);
+    VK.Auth.login(function (response) {
+        if (response.session) {
+            /* User is authorized successfully */
+            if (response.settings) {
+                /* Selected user access settings, if they were requested */
+            }
+        } else {
+            /* User clicked Cancel button in the authorization window */
+        }
+        console.log('response', response);
+        VK.Auth.getLoginStatus(function(data){
+            console.log('data', data);
+        });
+    }, 1);
 }
 $(document).ready(function(){
     $('.login-btn').on('click', function(){
